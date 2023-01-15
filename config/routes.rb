57888b0ef3a0af529425d15955aeb7a1b04d8ccc
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-01-15 14:20:06 +0000

    [add,devise]controller,view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers,skip: [:passwords] ,controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   root to: 'homes#top'
   get 'homes/about' => "homes#about",as: "about"
 end
