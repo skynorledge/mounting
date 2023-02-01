7d9212c1fe354a2a2cf957895953160d524e0472
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-02-01 11:57:11 +0000

    [update]ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,20 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items, only: [:new,:show,:index,:edit,:create,:update]
+    resources :genres, only: [:index,:edit,:create,:update]
+    resources :customers, only: [:show,:index,:edit,:update]
+    resources :orders, only: [:show,:update]
+    resources :order_items, only: [:update]
   end
 
   namespace :public do
     resources :items, only: [:show,:index]
+    resources :customers, only: [:show,:edit,:update]
+    resources :cart_items, only: [:update,:index,:destroy,:create]
+    resources :orders, only: [:new,:create,:index,:show]
+    resources :deliveries, only: [:index,:edit,:create,:update,:destroy]
   end
+
+
 
 end
