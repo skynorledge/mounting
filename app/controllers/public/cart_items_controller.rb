8d24c46afbaadc9ca308a39a9a81c4cbb081290d
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2023-02-20 08:38:10 +0000

    [period4]kadai

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,16 @@
 class Public::CartItemsController < ApplicationController
+  
+  
+   def create
+      binding.pry
+   end
+  
+  
+  
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end
